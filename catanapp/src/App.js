4120aa62500e38a83d013d84bee632f7895bf679
paulghodgson <paulghodgson@gmail.com>
2019-07-30 08:58:11 +0100

    added dice

--- catanapp/src/App.js
+++ catanapp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dice from './Components/Dice';
 
 function App() {
   return (
     <div>
-      <header >Catan Event Generator</header>      
+      <header>Catan Event Generator</header>
+      <Dice></Dice>      
     </div>
   );
 }
---
+++ catanapp/src/Components/Dice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Dice extends Component {
+    
+    render() {
+        return (
+            <div>
+                3
+                <button >Roll</button>
+            </div>
+        )
+    }
+}
