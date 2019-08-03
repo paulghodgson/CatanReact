8d68225347e0385d73c6d845842c2b59048adf97
paulghodgson <paulghodgson@gmail.com>
2019-08-03 10:47:46 +0100

    moved all dice logic into the app.js, this feels like I am making a god class wtf?

--- catanapp/src/App.js
+++ catanapp/src/App.js
@@ -8,10 +8,15 @@ export default class App extends Component {
 
   state = {      
       currentOutcomes: [{number: 2, occurrences: 0},{number: 3, occurrences: 0},{number: 4, occurrences: 0},{number: 5, occurrences: 0},{number: 6, occurrences: 0},{number: 7, occurrences: 0},{number: 8, occurrences: 0},{number: 9, occurrences: 0},{number: 10, occurrences: 0},{number: 11, occurrences: 0},{number: 12, occurrences: 0}],    
-      isNewYear: true,
+      isNewYear: false,
       rollCount:0,
+      roll:  this.rollDice() + this.rollDice()
   }
   
+  rollDice() {
+    return Math.ceil(Math.random() * 6);
+}
+
   constructor(props){
     super(props);
     this.recordRoll = this.recordRoll.bind(this);//binding the event hadnler so that state can change, this is a JS thing apparently
@@ -19,7 +24,29 @@ export default class App extends Component {
     this.janTheFirst = this.janTheFirst.bind(this);
   }
 
-  recordRoll(roll) {
+  recordRoll() {
+    const possibleOutcomes =  [{number: 2, combinations: 1}, {number:3, combinations:2}, {number:4, combinations:3}, {number:5, combinations:4}, {number:6, combinations:5},{number:7, combinations:6},{number:8, combinations:5}, {number:9,  combinations: 4}, {number:10,  combinations: 3}, {number:11,  combinations: 2}, {number:12,  combinations: 1} ];
+
+    let occurrences;    
+    let combinations;
+   
+    if(this.state.rollCount >= 31){
+        this.newYear();
+        return;
+    }
+    let roll;
+
+    do {  
+         roll = this.rollDice() + this.rollDice();
+        occurrences = this.state.currentOutcomes.find(item => item.number === roll).occurrences;
+        combinations = possibleOutcomes.find(item => item.number ===  roll).combinations;
+
+        console.log(combinations);
+    }while(occurrences >= combinations)
+
+    this.setState({roll: roll});  
+    this.setState((prevState, props) => {return {rollCount: prevState.rollCount + 1}});       
+
     const newOutcomes = [...this.state.currentOutcomes];
     newOutcomes.find(item => item.number === roll).occurrences++;
     this.setState({currentOutcomes: newOutcomes});   
@@ -39,8 +66,8 @@ export default class App extends Component {
     return (      
       <div style={{width: 800}}>     
           <Header/>
-          <Dice recordRoll={this.recordRoll}  currentOutcomes = {this.state.currentOutcomes} newYear={this.newYear} rollCount={this.state.rollCount}>/></Dice>   
-          <NewYear  isNewYear={this.state.isNewYear} janTheFirst={this.janTheFirst}     />
+          <Dice recordRoll={this.recordRoll} roll={this.state.roll} >/></Dice>   
+          <NewYear isNewYear={this.state.isNewYear} janTheFirst={this.janTheFirst}     />
       </div>
     );    
   }
--- catanapp/src/Components/Dice.js
+++ catanapp/src/Components/Dice.js
@@ -1,55 +1,15 @@
 import React, { Component } from 'react'
 
-export default class Dice extends Component {
-    
-    possibleOutcomes =  [{number: 2, combinations: 1}, {number:3, combinations:2}, {number:4, combinations:3}, {number:5, combinations:4}, {number:6, combinations:5},{number:7, combinations:6},{number:8, combinations:5}, {number:9,  combinations: 4}, {number:10,  combinations: 3}, {number:11,  combinations: 2}, {number:12,  combinations: 1} ];
-
-   
-    constructor(props){
-        super(props);
-    
-        this.state = {
-            roll: this.rollDice() + this.rollDice(),
-            rollCount: props.rollCount,
-        }    
-    }
+export default class Dice extends Component {    
 
     onClick = () => {
-        if(this.state.rollCount >= 31){
-            this.props.newYear();
-            return;
-        }
-
-        let occurrences;
-        let combinations;
-        let roll;
-        
-        function compare(number){
-            return number  === roll
-        };
-
-        do {
-             roll =  this.rollDice() + this.rollDice();            
-       
-            occurrences = this.props.currentOutcomes.find(item => compare(item.number)).occurrences;
-            combinations = this.possibleOutcomes.find(item => compare(item.number) ).combinations;
-    
-            console.log(combinations);
-        }while(occurrences >= combinations)
-
-        this.props.recordRoll(roll);
-        this.setState((prevState, props) => {return {rollCount: prevState.rollCount + 1}});
-        this.setState({roll: roll});  
-    }
-
-    rollDice() {
-        return Math.ceil(Math.random() * 6);
+        this.props.recordRoll();      
     }
 
     render() {
         return (
             <div style={{display: 'flex',   marginTop: 5}}>
-                <span style={{flex: "10", textAlign: "center", color: "#263940", fontSize:144}}>{this.state.roll}</span>
+                <span style={{flex: "10", textAlign: "center", color: "#263940", fontSize:144}}>{this.props.roll}</span>
                 <button onClick={this.onClick} style={{padding: 15, display: "inline-block", border:"none", flex: "1",background: "#c2e6f2", color: "#263940", fontSize:72}}>Roll</button>
             </div>
         )
