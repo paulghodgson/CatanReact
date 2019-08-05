2f77c7004c759cf1df1959681c118d85a46d1198
paulghodgson <paulghodgson@gmail.com>
2019-08-05 18:44:21 +0100

    all pretty now

--- catanapp/src/Components/EventStack.js
+++ catanapp/src/Components/EventStack.js
@@ -5,8 +5,8 @@ export default class EventStack extends Component {
     render() {
         return (
             <div>
-                <header>{this.props.name}</header>
-                <div>{this.props.description}</div>
+                <header style={{color: "#263940"}}><h1>{this.props.name}</h1></header>
+                <div style={{color: "#263940"}}>{this.props.description.split("\n").map((text, i) => i ? [<br/>, text] : text)}</div>
             </div>
         )
     }
